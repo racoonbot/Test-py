9cfb1a55b17384c2c911b6d5758919b696ab46d2
racoonbot <zozipaa@gmail.com>
2023-03-28 18:14:10 +0300

    Amon Commit

--- lessons.py
+++ lessons.py
@@ -1,12 +1,14 @@
 # user_data = int(input("Введи число:"))
-isHappy = False
+isHappy = True
 
 if isHappy:
     print("User is happy")
 else:
     print("Unhappy")
-
-# if user_data > 5:
-#     print("Wrong")
-# if user_data > 6:
-#     print("Sixty")
+    # if user_data > 5:
+    #  print("Wrong")
+    # if user_data > 6:
+    #  print("Sixty")
+x=input("Number:")
+if x == ( 3 ):
+    print( "3" )
