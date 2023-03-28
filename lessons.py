b5719bdcb6b7d967d4607e89af4bd8f618db4b57
racoonbot <zozipaa@gmail.com>
2023-03-28 20:37:54 +0300

    Thonny

--- lessons.py
+++ lessons.py
@@ -1,14 +1,18 @@
-# user_data = int(input("Введи число:"))
-isHappy = True
+user_data = int(input("Введи число:"))
+if user_data > 3:
+    print("Еще раз введи:")
+else: print("Ты куку")
+#     print("Ты котик") 
+# isHappy = True
 
-if isHappy:
-    print("User is happy")
-else:
-    print("Unhappy")
-    if user_data > 5:
-        print("Wrong")
-     if user_data > 6:
-         print("Sixty")
-x=input("Number:")
-if x == ( 3 ):
-    print( "3" )
+# if isHappy:
+#     print("User is happy")
+# else:
+#     print("Unhappy")
+#     if user_data > 5:
+#         print("Wrong")
+#     if user_data > 6:
+#          print("Sixty")
+# # x=input("Number:")
+# # if x == ( 3 ):
+# #     print( "3" )
