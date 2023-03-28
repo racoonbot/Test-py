c62426112e44e61e4c0970660d8ea0dc1b0145c0
racoonbot <zozipaa@gmail.com>
2023-03-28 16:13:28 +0300

    add lessons.py

---
+++ lessons.py
@@ -0,0 +1,12 @@
+# user_data = int(input("Введи число:"))
+isHappy = False
+
+if isHappy:
+    print("User is happy")
+else:
+    print("Unhappy")
+
+# if user_data > 5:
+#     print("Wrong")
+# if user_data > 6:
+#     print("Sixty")
