7c9a322d6d20baa587a9f48a2c99dbf136138b9e
racoonbot <zozipaa@gmail.com>
2023-03-28 22:00:52 +0300

    Spisok

--- lessons.py
+++ lessons.py
@@ -1,11 +1,11 @@
-user_data = int(input("Введи число:"))
-if user_data > 3:
-    print("Еще раз введи:")
-else: print("Ты куку")
-#     print("Ты котик") 
-# isHappy = True
-
-# if isHappy:
+# user_data = int(input("Введи число:"))
+# if user_data > 3:
+#     print("Еще раз введи:")
+# else: print("Ты куку")
+# #     print("Ты котик") 
+# # isHappy = True
+# 
+# # if isHappy:
 #     print("User is happy")
 # else:
 #     print("Unhappy")
@@ -13,6 +13,68 @@ else: print("Ты куку")
 #         print("Wrong")
 #     if user_data > 6:
 #          print("Sixty")
-# # x=input("Number:")
-# # if x == ( 3 ):
-# #     print( "3" )
+# #          print("Котик")
+# # # x=input("Number:")
+# # # if x == ( 3 ):
+# # #     print( "3" )
+# 
+# # nums = [53.6,True, 2, "kotik", [3,2]]
+# # nums[3] = 40
+# # nums[1] = False
+# # print(nums[-1][1])
+# 
+# numbers = [5, 7 , 100]
+# numbers.append(777)
+# # Добавляет в конец списка
+# numbers.insert(2,666)
+# # Вставляет (куда что)
+# x = [99,88,55]
+# numbers.extend(x)
+# # # Добавляет набор элементов
+# # numbers.sort()
+# # # Сортировка по порядку
+# # numbers.reverse()
+# # В обратном порядке
+# # numbers.pop()
+# # Удаляет последний элемент (по индексу(отрицательному)
+# numbers.remove(99)
+# # удаляет заданный элемент
+# # numbers.clear()
+# # удаляет всё
+# print(numbers.count(7))
+# # счетчик количества
+# print(len(numbers))
+#  длина списка
+# nums = [4,5,6,7,8,9, "sig", False]
+# for el in nums:
+#     el *= 2
+#     print(el)
+
+n = int(input("enter lenght: "))
+user_list = []
+i = 0
+while i < n:
+    string = "Elem No #" + str(i + 1) + ": "
+    user_list.append(input(string))
+    i += 1
+print(user_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
